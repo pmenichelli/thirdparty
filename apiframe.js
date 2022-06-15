a230efaa5bb53ee0d510592c21d0cc9e268c824f
Pablo Menichelli <meni.pablo@gmail.com>
2022-06-15 17:26:10 +0200

    Update apiframe.js

--- apiframe.js
+++ apiframe.js
@@ -1,9 +1,8 @@
 console.log('apiframe!');
 
 function makeFetchRequest() {
-  const options = {
-    method: 'POST',
-  }
+  
+  const options = JSON.parse(document.getElementById('fetchOptionsInput').value);
   fetch('https://safari-origin-null.glitch.me/', options)
     .then(response => console.log(response))
     .catch(err => console.log(err));
@@ -12,8 +11,9 @@ function makeFetchRequest() {
 function makeXHRRequest() {
   var http = new XMLHttpRequest();
   var url = 'https://safari-origin-null.glitch.me/';
-  http.open('POST', url, true);
 
+  http.open('POST', url, true);
+  http.withCredentials = true;
 
   http.onreadystatechange = function() {//Call a function when the state changes.
       if(http.readyState == 4 && http.status == 200) {
@@ -27,6 +27,16 @@ const fetchButton = document.createElement('button');
 fetchButton.innerText = 'Make Fetch request!';
 fetchButton.addEventListener('click', makeFetchRequest);
 
+const fetchOptionsInput = document.createElement('textarea');
+fetchOptionsInput.id = 'fetchOptionsInput';
+fetchOptionsInput.value = `{
+    method: 'POST,
+ }`;
+const fetchOptionsLabel = document.createElement('label');
+fetchOptionsLabel.for = 'fetchOptionsInput';
+
+document.body.appendChild(fetchOptionsLabel);
+document.body.appendChild(fetchOptionsInput);
 document.body.appendChild(fetchButton);
 
 const xhrButton = document.createElement('button');
@@ -34,3 +44,4 @@ xhrButton.innerText = 'Make XHR request!';
 xhrButton.addEventListener('click', makeXHRRequest);
 
 document.body.appendChild(xhrButton);
+
