580fd3b96ccca2db733b6ccc56dbd8f741a194d3
Pablo Menichelli <meni.pablo@gmail.com>
2022-06-15 14:26:10 +0200

    Update apiframe.js

--- apiframe.js
+++ apiframe.js
@@ -1,6 +1,6 @@
 console.log('apiframe!');
 
-function makeRequest() {
+function makeFetchRequest() {
   const options = {
     method: 'POST',
   }
@@ -9,8 +9,28 @@ function makeRequest() {
     .catch(err => console.log(err));
 }
 
-const button = document.createElement('button');
-button.innerText = 'Make request!';
-button.addEventListener('click', makeRequest);
+function makeXHRRequest() {
+  var http = new XMLHttpRequest();
+  var url = 'https://safari-origin-null.glitch.me/';
+  http.open('POST', url, true);
 
-document.body.appendChild(button);
+
+  http.onreadystatechange = function() {//Call a function when the state changes.
+      if(http.readyState == 4 && http.status == 200) {
+          console.log(http.responseText);
+      }
+  }
+  http.send();
+}
+
+const fetchButton = document.createElement('button');
+fetchButton.innerText = 'Make Fetch request!';
+fetchButton.addEventListener('click', makeFetchRequest);
+
+document.body.appendChild(fetchButton);
+
+const xhrButton = document.createElement('button');
+xhrButton.innerText = 'Make XHR request!';
+xhrButton.addEventListener('click', makeXHRRequest);
+
+document.body.appendChild(xhrButton);
